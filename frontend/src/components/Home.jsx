734aef4b697246c5e67b2dbdfbf851c3aea9b672
shreyash jain <sheryash123jain@gmail.com>
2024-04-06 16:01:17 +0530

    first

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomeNavbar from "./HomeNavbar"
+import Signup from "./Signup"
+import Login from "./Login"
+const Home = () => {
+   return(
+      <>
+      <HomeNavbar/>
+    <Signup/>
+   <Login/> 
+      </>
+   )
+
+}
+
+export default Home
